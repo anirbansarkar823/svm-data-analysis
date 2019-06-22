eb72ecbda2731605f19285f5f077f18563950d56
Kipa Tachak <ktachak@gmail.com>
2019-06-22 16:43:23 +0530

    Done for anganwadi cleaning

---
+++ python_files/cleaning_pipeline.py
@@ -0,0 +1,216 @@
+""" file: ./cleaning_pipeline.py
+A python module for creating a pipeline for the task of
+cleaning svm data.
+
+The sequence of events are as follows:
+    1. grouping columns to sections: Read indices from
+        YAML file and group columns into sections.
+    2. deletion of columns: Read indices from YAML file
+        delete columns from the dataframe.
+    3. maintain consistency between grouping and deletion.
+    4. cleaning of column entries: Match the column data
+        based on a general regex and then select 
+        formatting based on the contents.
+    5. manual checking for consistence: Left to the
+        admin.
+"""
+
+import re
+import yaml
+
+from collections import OrderedDict
+from collections import deque
+
+import pandas as pd
+
+
+FR_T_TIME = 'time'
+FR_T_MULT_SEL = 'multi_select'
+FR_T_STR_NR = 'normal_string'
+FR_T_NUM = 'number'
+
+REGEX = {
+        FR_T_TIME: '[0-9]{1,2}',
+        }
+
+
+yaml.add_representer(OrderedDict,
+        lambda dmpr, dt: dmpr.represent_dict(dt.items()))
+yaml.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        lambda ldr, nd: OrderedDict(ldr.construct_pairs(nd)))
+
+
+def to_yaml(fname, data):
+    with open(fname, 'w', encoding='utf-8') as f:
+        f.write(yaml.dump(data, default_flow_style=False))
+
+
+def from_yaml(fname):
+    with open(fname, 'r', encoding='utf-8') as f:
+        s = f.read()
+        return yaml.load(s)
+
+
+def del_cols(df, cols, inplace=False):
+    '''delete the columns from the dataframe *df* whose
+    column indices are given in *cols*. The changes will
+    be inplace or will return a modified copy base on the
+    flag *inplace*.
+
+    @df : input dataframe.
+    @cols : list containing the column indices.
+    @inplace : boolean flag.
+    @return : None if *inplace* True else None.
+    '''
+    df.drop(columns=cols, axis=1, inplace=inplace)
+
+
+
+def format_multi_choice_multi_select(df, col):
+    vals = [v for v in df[col].unique() if pd.notnull(v)]
+    for o_val in vals:
+        n_val = o_val.strip().lstrip('[').rstrip(']')
+        n_val = '#'.join([s.strip('"').strip().lower() \
+                        for s in n_val.split(',')])
+        df.loc[df[col]==o_val,col] = n_val
+
+
+def format_time(df, col):
+    vals = [v for v in df[col] if pd.notnull(v)]
+    for o_val in vals:
+        digit_list = re.findall(REGEX['time'], o_val)
+        digit_list = list(map(int, digit_list))
+        start_hr, start_min = digit_list[0], digit_list[1]
+        end_hr, end_min = digit_list[2], digit_list[3]
+        if end_hr < 12 and start_hr < 12 and start_hr < end_hr:
+            hours = end_hr-start_hr
+        elif end_hr < 12 and start_hr < 12 and end_hr < start_hr:
+            hours = 12+end_hr-start_hr
+        elif end_hr >= 12 and start_hr < 12:
+            hours = end_hr-start_hr
+        elif end_hr < 12 and start_hr >= 12:
+            hours = 24+end_hr-start_hr
+        else:
+            hours = 0
+        df.loc[df[col]==o_val,col] = 60*hours+(end_min-start_min)
+
+
+def format_normal_string(df, col):
+    vals = [v for v in df[col].unique() if pd.notnull(v)]
+    for v in vals:
+        if len(v) == 0:
+            df.loc[df[col]==v, col] = pd.np.nan
+        else:
+            df.loc[df[col]==v,col] = v.lower()
+
+
+FUNCS = {
+        FR_T_NUM: lambda df, col: None,
+        FR_T_MULT_SEL: format_multi_choice_multi_select,
+        FR_T_STR_NR: format_normal_string,
+        FR_T_TIME: format_time,
+        }
+
+
+def group_sec_and_format(df, secd):
+    '''group the columns in the dataframe *df* into
+    sections given in *secd*.
+
+    @df : input dataframe.
+    @secd : see the associated YAML format.
+    @return: *list*,*OrderedDict*, list of questions
+        and dictionary of grouping.
+    '''
+    grouped_sec, dq = OrderedDict(), deque()
+    dq.append((secd, grouped_sec))
+    ques_list = []
+
+    while len(dq) > 0:
+        scd, grpd = dq.pop()
+        for tag, tagd in scd.items():
+            if tagd.get('start'):
+                start, end = tagd['start'], tagd['end']
+                for ques in df.columns[start:end]:
+                    grpd[tag] = OrderedDict()
+                    grpd[tag][ques] = OrderedDict()
+                    ques_list.append(ques)
+                    if not(str(df[ques].dtype) in ['int64', 'float64']):
+                        vals = [v for v in df[ques].unique()
+                                if pd.notnull(v)]
+                        frmt = None
+                        if len(vals) > 0:
+                            if type(vals[0]) == float or type(vals[0]) == int:
+                                frmt = FR_T_NUM
+                            elif vals[0].strip().startswith('['):
+                                frmt = FR_T_MULT_SEL
+                            elif len(re.findall(REGEX[FR_T_TIME], vals[0])) == 4:
+                                frmt = FR_T_TIME
+                            else:
+                                frmt = FR_T_STR_NR
+                            grpd[tag][ques]['format'] = frmt
+                            FUNCS[frmt](df, ques)
+                        else:
+                            ques_list.pop()
+                    else:
+                        grpd[tag][ques]['format'] = FR_T_NUM
+            else:
+                grpd[tag] = OrderedDict()
+                dq.append((tagd,grpd[tag]))
+    return ques_list, grouped_sec
+
+
+def del_cols_group_sec_and_format(df, quesi, colsdel, secd, inplace=False):
+    '''delete and group columns.'''
+    ques_list, grouped_sec = group_sec_and_format(df, secd)
+    ques_del = (set(df.columns[quesi:]) - set(ques_list))
+    todelete = colsdel + list(ques_del)
+    del_cols(df, todelete, inplace)
+    return ques_list, grouped_sec
+
+
+def clean(df, attrd):
+    filter_attr = attrd['filter']['attribute']
+    filter_val = attrd['filter']['value']
+    df = df.loc[df[filter_attr] == filter_val, :].copy()
+    ques_i = attrd['questions_starting_index']
+    grouping = attrd['grouping']
+    colsdel = [df.columns[i] for i in attrd['delete_cols']]
+
+    ques_list, groupedd = del_cols_group_sec_and_format(df, ques_i, colsdel, grouping, inplace=True)
+
+    labels = ['correct', 'rename', 'attr_seq', 'reformat', 'sections']
+    values = ['# to be filled manually #' for i in range(4)] + [groupedd]
+    return OrderedDict(zip(labels, values)),df
+
+
+if __name__ == '__main__':
+    import os
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Clean data in the input csv file and generate output csv and yaml files.")
+    parser.add_argument('-df', help='Path to the input csv file')
+    parser.add_argument('-yml', help='Path to the input yaml template.')
+    parser.add_argument('-s', help='Separator used in the csv file')
+
+    args = parser.parse_args()
+
+    infile, sep, ymlfile = args.df, args.s, args.yml
+    filename = infile.split('.')[0]
+
+    out_yaml_file = filename+'_attr_template.yaml'
+    out_csv_file = filename+'_processed.csv'
+
+    out_yaml_exists = os.path.isfile(out_yaml_file)
+    out_csv_exists = os.path.isfile(out_csv_file)
+
+    if out_yaml_exists:
+        print(out_yaml_file, 'exists!')
+    elif out_csv_exists:
+        print(out_csv_file, 'exists!')
+    else:
+
+        df = pd.read_csv(infile, sep=sep)
+        attrd = from_yaml(ymlfile)
+        groupped, df = clean(df, attrd)
+        df.to_csv(out_csv_file, sep=',', na_rep='N/A')
+        to_yaml(out_yaml_file, groupped)
---
+++ python_files/gen_clean_final.py
@@ -0,0 +1,119 @@
+import itertools
+
+import pandas as pd
+
+from collections import OrderedDict, deque
+
+from cleaning_pipeline import (
+        FR_T_STR_NR, FR_T_TIME, FR_T_MULT_SEL, FR_T_NUM)
+from cleaning_pipeline import to_yaml, from_yaml
+
+
+def reformat_df(df, col, frmt):
+    if frmt == FR_T_TIME or FR_T_NUM:
+        df.loc[:,col] = pd.to_numeric(df[col])
+
+
+REFORMAT = {
+        FR_T_STR_NR : lambda df, col: None,
+        FR_T_NUM: lambda df, col: reformat_df(df, col, FR_T_NUM),
+        FR_T_TIME: lambda df, col: None,
+        FR_T_MULT_SEL: lambda df, col: None,
+        }
+
+TAG_FOR_FRMT = {
+        FR_T_STR_NR: 'values',
+        FR_T_MULT_SEL: 'values',
+        FR_T_NUM: 'threshold',
+        FR_T_TIME: 'threshold',
+        }
+
+STR_FOR_TAG = {
+        'values' : '# fill with value #',
+        'threshold': '# fill with threshold #',
+        }
+
+
+def correct_df(df, cols_correct_dat):
+    for col, corr_dat in cols_correct_dat.items():
+        o_val = list(corr_dat.keys())[0]
+        df.loc[df[col] == o_val,col] = corr_dat[o_val]
+
+def rename_cols(df, colsd):
+    df.rename(columns=colsd, inplace=True)
+
+
+def gen_final_yaml_for_weights(df, attrd, attrseq, refrmtd):
+    weightsd, dq = OrderedDict(), deque()
+    dq.append((0, attrd, weightsd))
+    while len(dq) > 0:
+        lvl, atrd, nptr = dq.pop()
+        label = attrseq[lvl]
+        if len(atrd) <= 0:
+            continue
+
+        nptr[label] = OrderedDict()
+        for tag, tagd in atrd.items():
+            frmt = tagd.get('format')
+            if frmt:
+                reformat = refrmtd.get(tag)
+                frmt = reformat or frmt
+                REFORMAT[frmt](df, tag)
+                inner_tag = TAG_FOR_FRMT[frmt]
+                val_str = STR_FOR_TAG[inner_tag]
+                nptr[label][tag] = OrderedDict()
+                nptr[label][tag]['weight'] = '# fill with weights #'
+                nptr[label][tag][inner_tag] = val_str
+                if frmt in [FR_T_STR_NR, FR_T_MULT_SEL]:
+                    vals = set(v for v in df[tag].unique()
+                            if pd.notnull(v))
+                    if frmt == FR_T_MULT_SEL:
+                        join_func = itertools.chain.from_iterable
+                        vals = set(join_func(v.split('#') for v in vals))
+                    valued = OrderedDict((v, val_str) for v in vals)
+                    nptr[label][tag][inner_tag] = valued
+            else:
+                nptr[label][tag] = OrderedDict()
+                nptr[label][tag]['weight'] = '# fill with weights #'
+                dq.append((lvl+1,tagd, nptr[label][tag]))
+
+    return weightsd
+
+
+if __name__ == '__main__':
+    import argparse
+    import os
+    
+    parser = argparse.ArgumentParser(description='Correct data in the processed csv data and generated YAML file for weights assignment.')
+    parser.add_argument('-df', help='Path to the processed csv file to be corrected')
+    parser.add_argument('-yml', help='Path to the YAML file with template and correction data.')
+    parser.add_argument('-s', help='Separator used in the csv file.')
+
+    args = parser.parse_args()
+
+    infile, sep, ymlfile = args.df, args.s, args.yml
+    filename = infile.split('.')[0]
+    out_yaml_file = filename+'_weights_thresh_vals.yaml'
+    out_csv_file = filename+'_cleaned.csv'
+
+    out_yaml_exists = os.path.isfile(out_yaml_file)
+    out_csv_exists = os.path.isfile(out_csv_file)
+
+    if out_yaml_exists:
+        print(out_yaml_file, 'exists!')
+    elif out_csv_exists:
+        print(out_csv_file, 'exists!')
+    else:
+        df = pd.read_csv(infile, sep=sep)
+        attrd_corr = from_yaml(ymlfile)
+        corr_data = attrd_corr['correct']
+        colsd = attrd_corr['rename']
+        attr_seq = attrd_corr['attr_seq']
+        reformatd = attrd_corr['reformat']
+        attrd = attrd_corr['sections']
+
+        correct_df(df, corr_data)
+        rename_cols(df, colsd)
+        df.to_csv(out_csv_file, sep=',', na_rep='N/A')
+        to_yaml(out_yaml_file,
+                gen_final_yaml_for_weights(df, attrd, attr_seq, reformatd))
